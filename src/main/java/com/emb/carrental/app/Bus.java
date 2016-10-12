a7e3f8b51fb8fc6f33eba99e2609f3efe63a65da
bu_000 <bu_000@bu.Home>
2016-10-12 04:24:04 +0100

    Vehicle and Bus Commit

---
+++ src/main/java/com/emb/carrental/app/Bus.java
@@ -0,0 +1,54 @@
+package com.emb.carrental.app;
+
+import com.emb.carrental.intf.Rentable;
+import com.emb.carrental.util.Database;
+import com.emb.carrental.util.InvalidDistanceException;
+import com.emb.carrental.util.Trip;
+import com.emb.carrental.util.Util;
+import java.io.Serializable;
+
+/**
+ *
+ * @author bu_000
+ */
+public class Bus extends Vehicle implements Rentable, Serializable {
+
+    private double discountRate = 0.02;
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    @Override
+    public void rent() {
+        System.out.println("Not yet Implemented");
+    }
+
+    /**
+     *
+     * @param trip
+     * @return
+     * @throws InvalidDistanceException Buses receive 2% discount on standard
+     * Rate
+     */
+    @Override
+    public double calculateTotalExpenseForTrip(Trip trip) throws InvalidDistanceException {
+        this.type = trip.getType();
+        this.noOfPassengers = trip.getNoOfPassengers();
+        rate = new Rate(trip.getEngineType(), isAC());
+        double standardRate = rate.getStandardRate();
+        rate.setStandardRate(standardRate - (standardRate * discountRate));
+        double totalDistance = Database.calculateTotalDistance(trip.getDistances());
+        double additionalCharge = 0;
+
+        if (limitIsExceeded()) {
+            additionalCharge = rate.getAdditionalChargeRatePerKmAndPerson() * totalDistance * noOfPassengers;
+        }
+        double totalExpenseForTrip = (rate.getStandardRate() * totalDistance) + additionalCharge;
+        return Util.roundOff(totalExpenseForTrip, 2);
+    }
+}
