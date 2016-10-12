bf6596f89b1a85b215bfc5b53f3c53dc310e64a7
bu_000 <bu_000@bu.Home>
2016-10-12 04:20:07 +0100

    Refactored Vehicle Class to use Rate Object which may vary based on Vehicle attributes.
    Introduces a check to determine if Vehicle has AC or NON AC 
    so as to apply the neccessary rates or discounts required.
    
    Vehicle is Modelled as at the top of the Inheritance Hierarchy
    so exposes some protected state for subclasses
    
    Introduced a polymorphic method which may be overriden by 
    Vehicle subclasses. Provided a default implementation based on business policies.
    
    Coded to business rules using domain driven approach that 
    hides the implementation of rates calculation here and allows Rate 
    Object control and deal with rates Application but for the additional charge if limitIsExceeded since its required for adding up 
    the total rate within the calculateTotalExpenseForTrip method.
    .
    
    Created a Bus Vehicle Sub-class to capture discount rate difference
    and override the implementation of Vehicle
    
    Set up a test case which requires I create a few more Vehicle sub classes
    to test it properly Since Vehicle can not be instantiated.

--- src/test/java/com/emb/carrental/app/VehicleIT.java
+++ src/test/java/com/emb/carrental/app/VehicleIT.java
@@ -1,9 +1,21 @@
 package com.emb.carrental.app;
 
+import com.emb.carrental.util.Trip;
+import org.junit.Before;
+
 /**
  *
  * @author bu_000
  */
 public class VehicleIT {
+
+    private Trip trip;
+    private Vehicle vehicle;
+
+    @Before
+    public void initClient() {
+        this.trip = new Trip();
+        this.trip.setTrip("Pune-Mumbai-Bangalore-Pune");
+    }
 
 }
