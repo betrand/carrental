a495d423d2f04fc390f966649edd1760c17c537c
bu_000 <bu_000@bu.Home>
2016-10-12 08:55:56 +0100

    Expose CarRentalResource Result as JSON and expose QueryStrings. Modified CarRentalResource Test Case to send Trip Info in Query String
    Tested calls from Browser Client
    
    Cleaned up some codes

--- src/main/java/com/emb/carrental/res/CarRentalResource.java
+++ src/main/java/com/emb/carrental/res/CarRentalResource.java
@@ -1,12 +1,18 @@
 package com.emb.carrental.res;
 
-import com.emb.carrental.app.Swift;
-import com.emb.carrental.app.Vehicle;
+import com.emb.carrental.app.control.RentalService;
 import com.emb.carrental.util.InvalidDistanceException;
+import com.emb.carrental.util.LOG;
 import com.emb.carrental.util.Trip;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -17,24 +23,50 @@ import javax.ws.rs.core.Response;
 public class CarRentalResource {
 
     @GET
-    @Path("calculateTotalExpense")
-    public String calculateTotalExpense() {
-        Trip trip_ = new Trip();
-        trip_.setVehicleType("Swift");
-        trip_.setEngineType("Diesel");
-        trip_.setType("NON AC");
-        trip_.setTrip("Pune-Mumbai-Bangalore-Pune"); // total distance 1600
-        trip_.setNoOfPassengers(3);
+    @Path("calculateTotalExpense/{vehicleType}/{engineType}/{actype}/{trip}/{noOfPassengers}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculateTotalExpense(
+            @PathParam("vehicleType") String vehicleType,
+            @PathParam("engineType") String engineType,
+            @PathParam("actype") String type,
+            @PathParam("trip") String trip,
+            @PathParam("noOfPassengers") String noOfPassengers) {
 
-        Vehicle vehicle = new Swift();
+        LOG.info("calculateTotalExpense", vehicleType);
+
+        Trip trip_ = new Trip();
+        trip_.setVehicleType(vehicleType);
+        trip_.setEngineType(engineType);
+        trip_.setType(type);
+        trip_.setTrip(trip);
+        trip_.setNoOfPassengers(Integer.parseInt(noOfPassengers));
+
         double totalExpenseForTrip = 0;
         try {
-            totalExpenseForTrip = vehicle.calculateTotalExpenseForTrip(trip_);
-            System.out.println("totalExpenseForTrip : " + totalExpenseForTrip);
+            RentalService rentalService = new RentalService();
+            totalExpenseForTrip = rentalService.calculateTotalExpenseForTrip(trip_);
         } catch (InvalidDistanceException ide) {
-            throw new WebApplicationException(ide.getMessage(), Response.Status.NO_CONTENT);
+            throw new WebApplicationException("Invalid Distance", Response.Status.NO_CONTENT);
         }
-        return String.valueOf(totalExpenseForTrip); //Response.ok(expense).build();
+        LOG.info("totalExpenseForTrip", totalExpenseForTrip);
+        return String.valueOf(totalExpenseForTrip);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Path("calculateTotalExpenses")
+    public JsonObject calculateTotalExpenses(Trip trip_) {
+        LOG.info("calculateTotalExpenses", trip_);
+
+        double totalExpenseForTrip = 0;
+        try {
+            RentalService rentalService = new RentalService();
+            totalExpenseForTrip = rentalService.calculateTotalExpenseForTrip(trip_);
+        } catch (InvalidDistanceException ide) {
+            throw new WebApplicationException("Invalid Distance", Response.Status.NO_CONTENT);
+        }
+        return Json.createObjectBuilder().add("TotalExpense", totalExpenseForTrip).build();
     }
 
 }
