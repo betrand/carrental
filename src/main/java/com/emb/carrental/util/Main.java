88f76a58e95575e3df51c132ee9a61a078d6dc74
bu_000 <bu_000@bu.Home>
2016-10-12 09:26:37 +0100

    Added Packaging Config to pom

--- src/main/java/com/emb/carrental/util/Main.java
+++ src/main/java/com/emb/carrental/util/Main.java
@@ -2,6 +2,7 @@ package com.emb.carrental.util;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Optional;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -13,8 +14,22 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class Main {
 
     // Base URI the Grizzly HTTP server will listen on
-    public static final String API_PATH = "http://localhost:8888/CarRental/api/";
+    public static final String API_PATH;
+    public static final String protocol;
+    public static final Optional<String> host;
+    public static final String path;
+    public static final Optional<String> port;
 
+    static {
+        protocol = "http://";
+        host = Optional.ofNullable(System.getenv("HOSTNAME"));
+        port = Optional.ofNullable(System.getenv("PORT"));
+        path = "CarRental/api";
+        API_PATH = protocol + host.orElse("localhost") + ":" + port.orElse("8888") + "/" + path + "/";
+    }
+
+    // Base URI the Grizzly HTTP server will listen on
+    //public static final String API_PATH = "http://localhost:8888/CarRental/api/";
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.emb.carrental.res package
